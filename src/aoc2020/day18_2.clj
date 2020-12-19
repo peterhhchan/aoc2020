b13dcc9e95e47c056c78ed645d918eafdc2ee31d
peterhhchan <peterhhchan@gmail.com>
2020-12-19 05:44:35 -0500

    Day19-1

--- src/aoc2020/day18_2.clj
+++ src/aoc2020/day18_2.clj
@@ -1,4 +1,4 @@
-(ns aoc2020.day18
+(ns aoc2020.day18-2
   (:require [clojure.string :as s]))
 
 (defn data []
@@ -19,15 +19,14 @@
         s
         (find-op s)
         (let [[_ m]     (find-op s)
-              [d1 op d2] (s/split m #" ")
-              f          (get {"+" + "*" *} op)]
-          (solve-inner-1 (s/replace-first s m (str (f (toLong d1) (toLong d2)))))
-#_          (solve-inner-1 (s/replace-first s m (str (* (toLong d1) (toLong d2))))))))
+              [d1 op d2] (s/split m #" ")]
+          (if (= op "+")
+            (solve-inner-1 (s/replace-first s m (str (+ (toLong d1) (toLong d2)))))            (solve-inner-1 (s/replace-first s m (str (* (toLong d1) (toLong d2)))))))))
 
 (defn solve [f s]
   (if-let [[_ m] (match-inner s)]
     (let [exp (subs m 1 (dec (count m)))]
-      (solve f (s/replace s m (f exp))))
+      (solve-2 f (s/replace s m (f exp))))
     (f s)))
 
 (defn part-1 []
@@ -48,13 +47,6 @@
         (let [[_ m]     (re-find #".*?([\d]+ [*] [\d]+).*" s)
               [d1 _ d2] (s/split m #" ")]
           (solve-inner-2 (s/replace-first s m (str (* (toLong d1) (toLong d2))))))))
-
-(def input ["1 + 2 * 3 + 4 * 5 + 6"
-            "1 + (2 * 3) + (4 * (5 + 6))"
-            "2 * 3 + (4 * 5)"
-            "5 + (8 * 3 + 9 + 3 * 4 * 3)"
-            "5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))"
-            "((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2"])
 
 (defn part-2 []
   (->> (data)
--- src/aoc2020/day19.clj
+++ src/aoc2020/day19.clj
@@ -1,9 +1,18 @@
 (ns aoc2020.day19
-  (:require [clojure.string :as s]))
+  (:require [clojure.string :as s]
+            [clojure.string :as str]))
 
 (defn data []
-  (->> (slurp "data/aoc2020_day19.txt")
-       s/split-lines))
+  (-> (slurp "data/aoc2020_day19.txt")
+       (s/split #"\n\n")))
+
+(def test-input
+  ["0: 4 1 5"
+   "1: 2 3 | 3 2"
+   "2: 4 4 | 5 5"
+   "3: 4 5 | 5 4"
+   "4: \"a\""
+   "5: \"b\""])
 
 (defn toInt [s]
   (Integer/parseInt s))
@@ -11,8 +20,120 @@
 (defn toLong [s]
   (Long/parseLong s))
 
+(defn isLetter? [s]
+  (re-find #"\"([ab]+)\"" s))
+
+(defn merge-rule
+  ([lib ]
+   (merge-rule lib 0))
+
+  ([lib rid]
+   (let [rules (get lib rid "")]
+     (if (string? rules)
+       [rules]
+       (->> rules
+            (mapcat (fn [rule]
+                      (cond  (= 1 (count rule))
+                             (merge-rule lib (get rule 0))
+                             (= 2 (count rule))
+                             (into []
+                                   (let [r1 (merge-rule lib (get rule 0))
+                                         r2 (merge-rule lib (get rule 1))]
+                                     (for [x r1
+                                           y r2]
+                                       (str x y))))
+#_                             (= 3 (count rule))
+#_
+                             (into []
+                                   (for [x (merge-rule lib (get rule 0))
+                                         y (merge-rule lib (get rule 1))
+                                         z (merge-rule lib (get rule 2))]
+                                     (str x y z)))))))))))
+
 (defn part-1 []
-  (let [d (data)]))
+  (let [[rules msgs] (data)
+        lib (->> rules
+                 (s/split-lines)
+                 (map (fn [rule]
+                        (let [[id rs] (s/split rule #": ")
+                              rid (toInt id)]
+                          {rid
+                           (if (isLetter? rs)
+                             (second (isLetter? rs))
+                             (->>  (s/split rs #" \| ")
+                                   (mapv (fn [r]
+                                          (->> (s/split r #" ")
+                                               (mapv toLong))))))})))
+                 (into {}))
+        all-rules (set (merge-rule lib 0))]
+    (->> msgs
+         (s/split-lines)
+         (filter all-rules)
+         count)))
+
 
 (defn part-2 []
-  (let [d (data)]))
+  (let [[rules msgs] (data)
+        lib (->> rules
+                 (s/split-lines)
+                 (map (fn [rule]
+                        (let [[id rs] (s/split rule #": ")]
+                          {(toInt id)
+                           (if (isLetter? rs)
+                             (second (isLetter? rs))
+                             (->>  (s/split rs #" \| ")
+                                   (mapv (fn [r]
+                                          (->> (s/split r #" ")
+                                               (mapv toLong))))))})))
+                 (into {}))
+        new-rule (set (merge-rule lib 8))
+        all-rules (set (merge-rule lib 0))
+        unmatched     (->> msgs
+                     (s/split-lines)
+                     (remove all-rules))]
+    (->>     msgs
+             (s/split-lines)
+             (filter all-rules))
+#_    (->> unmatched
+         (filter (fn [s]
+                (some #(str/includes? s %) new-rule)))
+         count)))
+
+
+#_(defn part-2 []
+  (let [[rules msgs] (data)
+        lib (->> rules
+                 (s/split-lines)
+                 (map (fn [rule]
+                        (let [[id rs] (s/split rule #": ")
+                              rid (toInt id)]
+                          {rid
+                           (if (isLetter? rs)
+                             (second (isLetter? rs))
+                             (->>  (s/split rs #" \| ")
+                                   (mapv (fn [r]
+                                          (->> (s/split r #" ")
+                                               (mapv toLong))))))})))
+                 (into {}))
+        ra (set (merge-rule lib 8))
+        rb (set (merge-rule lib 42))
+        rc (set (merge-rule lib 31))
+
+        r-all (set (merge-rule lib 0))
+        max-length (apply max (map count r-all))]
+
+
+    (->> msgs
+         (s/split-lines)
+         (filter (fn [s]
+                   (or (r-all s)
+                       (some? (map #(r-all (str % s)) ra))
+                       (seq (->> (for [x rb
+                                       y rc]
+                                   (str x s y))
+                                 (filter r-all)
+                                 (remove nil?))))))
+         (count)
+)
+
+))

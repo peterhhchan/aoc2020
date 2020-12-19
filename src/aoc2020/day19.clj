f00fdb64abf0c2eef5f258456c9831105bd14723
peterhhchan <peterhhchan@gmail.com>
2020-12-19 07:57:39 -0500

    Day 19 part 2

--- src/aoc2020/day19.clj
+++ src/aoc2020/day19.clj
@@ -1,6 +1,7 @@
 (ns aoc2020.day19
   (:require [clojure.string :as s]
-            [clojure.string :as str]))
+            [clojure.string :as str]
+            [clojure.set :as set]))
 
 (defn data []
   (-> (slurp "data/aoc2020_day19.txt")
@@ -42,98 +43,61 @@
                                      (for [x r1
                                            y r2]
                                        (str x y))))
-#_                             (= 3 (count rule))
-#_
+                             (= 3 (count rule))
                              (into []
                                    (for [x (merge-rule lib (get rule 0))
                                          y (merge-rule lib (get rule 1))
                                          z (merge-rule lib (get rule 2))]
                                      (str x y z)))))))))))
 
+(defn rules-dict [rules]
+  (->> rules
+       (s/split-lines)
+       (map (fn [rule]
+              (let [[id rs] (s/split rule #": ")
+                    rid (toInt id)]
+                {rid
+                 (if (isLetter? rs)
+                   (second (isLetter? rs))
+                   (->>  (s/split rs #" \| ")
+                         (mapv (fn [r]
+                                 (->> (s/split r #" ")
+                                      (mapv toLong))))))})))
+       (into {})))
+
 (defn part-1 []
   (let [[rules msgs] (data)
-        lib (->> rules
-                 (s/split-lines)
-                 (map (fn [rule]
-                        (let [[id rs] (s/split rule #": ")
-                              rid (toInt id)]
-                          {rid
-                           (if (isLetter? rs)
-                             (second (isLetter? rs))
-                             (->>  (s/split rs #" \| ")
-                                   (mapv (fn [r]
-                                          (->> (s/split r #" ")
-                                               (mapv toLong))))))})))
-                 (into {}))
-        all-rules (set (merge-rule lib 0))]
+        lib          (rules-dict rules)
+        all-rules    (set (merge-rule lib 0))]
     (->> msgs
          (s/split-lines)
          (filter all-rules)
          count)))
 
-
 (defn part-2 []
   (let [[rules msgs] (data)
-        lib (->> rules
-                 (s/split-lines)
-                 (map (fn [rule]
-                        (let [[id rs] (s/split rule #": ")]
-                          {(toInt id)
-                           (if (isLetter? rs)
-                             (second (isLetter? rs))
-                             (->>  (s/split rs #" \| ")
-                                   (mapv (fn [r]
-                                          (->> (s/split r #" ")
-                                               (mapv toLong))))))})))
-                 (into {}))
-        new-rule (set (merge-rule lib 8))
+        lib          (rules-dict rules)
+        heads     (set (merge-rule lib 42))
+        tails     (set (merge-rule lib 31))
         all-rules (set (merge-rule lib 0))
-        unmatched     (->> msgs
-                     (s/split-lines)
-                     (remove all-rules))]
-    (->>     msgs
-             (s/split-lines)
-             (filter all-rules))
-#_    (->> unmatched
-         (filter (fn [s]
-                (some #(str/includes? s %) new-rule)))
-         count)))
+        len       (count (first heads))]
 
-
-#_(defn part-2 []
-  (let [[rules msgs] (data)
-        lib (->> rules
-                 (s/split-lines)
-                 (map (fn [rule]
-                        (let [[id rs] (s/split rule #": ")
-                              rid (toInt id)]
-                          {rid
-                           (if (isLetter? rs)
-                             (second (isLetter? rs))
-                             (->>  (s/split rs #" \| ")
-                                   (mapv (fn [r]
-                                          (->> (s/split r #" ")
-                                               (mapv toLong))))))})))
-                 (into {}))
-        ra (set (merge-rule lib 8))
-        rb (set (merge-rule lib 42))
-        rc (set (merge-rule lib 31))
-
-        r-all (set (merge-rule lib 0))
-        max-length (apply max (map count r-all))]
-
+    (prn (count (first heads)))
+    (prn (count (s/split-lines msgs)))
+    (prn (count (set/intersection heads tails)))
 
     (->> msgs
          (s/split-lines)
-         (filter (fn [s]
-                   (or (r-all s)
-                       (some? (map #(r-all (str % s)) ra))
-                       (seq (->> (for [x rb
-                                       y rc]
-                                   (str x s y))
-                                 (filter r-all)
-                                 (remove nil?))))))
-         (count)
-)
-
-))
+         (map (fn [msg]
+                (->> (partition len msg)
+                     (map #(let [ss (apply str %)]
+                             (cond (heads ss) :a
+                                   (tails ss) :b
+                                   :else nil)))
+                     (partition-by identity))))
+         (filter (fn [msg]
+                   (and (= 2 (count msg))
+                        (= :a (ffirst msg))
+                        (= :b (first (last msg)))
+                        (> (count (first msg)) (count (second msg))))))
+         (count))))

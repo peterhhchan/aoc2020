94d0e2e0f836b5aafb27d879ecbdb36384f5a7de
peterhhchan <peterhhchan@gmail.com>
2020-12-21 02:09:24 -0500

    Day21

--- src/aoc2020/day20.clj
+++ src/aoc2020/day20.clj
@@ -14,7 +14,7 @@
 
 (defn rotate-grid [g] (apply mapv vector (reverse g)))
 (defn transpose-grid [g] (apply mapv vector g))
-(defn all-rotations [g]
+(defn all-positions [g]
   (let [rots  (->> (iterate rotate-grid g)
                    (take 4))]
     (concat rots (map transpose-grid rots))))
@@ -26,7 +26,7 @@
    :left    (border-id (map first image))})
 
 (defn all-sides [image]
-  (->> (all-rotations image)
+  (->> (all-positions image)
        (map borders)
        (mapcat (juxt :top :bottom :right :left))
        set))
@@ -43,7 +43,7 @@
 (->> (images)
      (map s/split-lines)
      (map (fn [[header & img]]
-            {:id    (toInt (second (re-find #"Tile ([\d]+)\:" header)))
+            {:id    (toInt (first (re-seq #"[\d]+" header)))
              :image (mapv vec img)
              :edges (all-sides (mapv vec img))}))
      (into #{})))
@@ -59,8 +59,8 @@
          (map :id)
          (reduce *))))
 
-(defn all-images [{:keys [id image edges]}]
-  (->> (all-rotations image)
+(defn all-image-positions [{:keys [id image edges]}]
+  (->> (all-positions image)
        (map #(merge {:id id
                      :edges edges
                      :image %}
@@ -80,24 +80,23 @@
         grid
         (if (< y 11)
           (let [m         (first (filter #(get (:edges %) top) tiles))
-                next-tile (->> (all-images m)
+                next-tile (->> (all-image-positions m)
                                (filter #(= (:bottom %) top))
                                first)]
-            (if next-tile
+            (when next-tile
               (recur (remove-tile tiles next-tile)
                      (merge grid {[x (inc y)] next-tile})
                      (:top next-tile) left
                      x (inc y))))
           (let [m         (first (filter #(get (:edges %) left) tiles))
-                next-tile (->> (all-images m)
+                next-tile (->> (all-image-positions m)
                                (filter #(= (:right  %) left))
                                first)]
-            (if next-tile
+            (when next-tile
               (recur (remove-tile tiles next-tile)
                      (merge grid {[(inc x) 0] next-tile})
                      (:top next-tile) (:left next-tile)
-                     (inc x) 0)
-                  grid))))))
+                     (inc x) 0)))))))
 
 (defn stich-images [g]
   (->>  (for [y (range 12)]
@@ -119,47 +118,45 @@
 
 (defn offsets [monster]
   (->>   (for [x (range (count (first monster)))
-               y (range (count monster))]
-           (when (= \# (get-in monster [y x]))
-             [x y]))
+               y (range (count monster))
+               :when (= \# (get-in monster [y x]))]
+           [x y])
          (remove nil?)))
 
 (defn monster-offsets []
-  (map offsets (all-rotations monster)))
+  (map offsets (all-positions monster)))
 
 (defn base-roughness [picture]
   (->> (for [x (range (count (first picture)))
-             y (range (count (first picture)))]
-         (when (= \# (get-in picture [x y]))
-           [x y]))
-       (remove nil?)
+             y (range (count (first picture)))
+             :when (= \# (get-in picture [x y]))]
+         [x y])
        (into #{})))
 
 (defn part-2 []
   (let [tiles         (all-tiles)
         all-neighbors (all-neighboring-edges tiles)
         start-tile    (->> (corners tiles all-neighbors)
-                              first
-                              (all-images)
-                              (filter #(all-neighbors (:left %)))
-                              (filter #(all-neighbors (:top %)))
+                           first
+                           (all-image-positions)
+                           (filter #(all-neighbors (:left %)))
+                           (filter #(all-neighbors (:top %)))
                               ;; There are two possible starting positions for the corner
-                              first)
+                           first)
         image         (solve-image tiles all-neighbors start-tile)
         picture       (stich-images image)
-        roughness     (base-roughness picture)
         monsters      (monster-offsets)]
+;;(map (partial apply str) (map (partial take 60) (take 100 (picture))))
     (->>  (for [x (range (count (first picture)))
                 y (range (count picture))]
-            (->> monsters
-                 (map (fn [monster]
-                        (let [pos (map (fn [[a b]] [(+ a x) (+ b y)]) monster)]
-                           (when (every? #(= \# (get-in picture [(first %) (second %)])) pos)
-                             (into #{} pos)))))
-                 (remove empty?)
-                 (remove nil?)
-                 (apply clojure.set/union)))
-          (remove empty?)
-          (apply clojure.set/union)
-          count
-          (- (count roughness)))))
+              (->> monsters
+                   (map (fn [monster]
+                          (let [pos (map (fn [[a b]] [(+ a x) (+ b y)]) monster)]
+                            (when (every? #(= \# (get-in picture %)) pos)
+                              (into #{} pos)))))
+                   (remove nil?)
+                   (apply clojure.set/union)))
+            (remove empty?)
+            (apply clojure.set/union)
+            count
+            (- (count (base-roughness picture))))))
--- src/aoc2020/day21.clj
+++ src/aoc2020/day21.clj
@@ -1,18 +1,65 @@
 (ns aoc2020.day21
-  (:require [clojure.string :as s]))
+  (:require [clojure.string :as s]
+            [clojure.string :as str]))
 
 (defn data []
   (->> (slurp "data/aoc2020_day21.txt")
        s/split-lines))
 
-(defn toInt [s]
-  (Integer/parseInt s))
+#_(defn data []
+  ["mxmxvkd kfcds sqjhc nhms (contains dairy, fish)"
+   "trh fvjkl sbzzf mxmxvkd (contains dairy)"
+   "sqjhc fvjkl (contains soy)"
+   "sqjhc mxmxvkd sbzzf (contains fish)"])
 
-(defn toLong [s]
-  (Long/parseLong s))
+
+
+(defn compare-foods [f1 f2 k]
+  (clojure.set/intersection (k f1) (k f2)))
+
+(defn reduce-foods [d]
+    (->> (for [x (range (count d))
+               y (range (count d))]
+           (when (seq (compare-foods (d x)(d y) :allergens))
+             [(compare-foods (d x)(d y) :allergens)
+              (compare-foods (d x)(d y) :foods)]))
+         (remove nil?)
+         (group-by first)
+         (mapv (fn [[k v]]
+                {:allergens k
+                 :foods (apply clojure.set/intersection (map second v))}))))
 
 (defn part-1 []
-  (let [d (data)]))
+  (let [d (->> (data)
+               (mapv (fn [l]
+                      (let [allergen-list (re-find #"(\(.*\))+" l)
+                            foods      (re-seq #"[\w]+ " l)
+                            allergens    (re-seq #" [\w]+" (last allergen-list))]
+                        {:foods (disj (set foods) "contains ")
+                         :allergens (set allergens)}))))
+        allergens
+        (loop [mm (reduce-foods d)
+               mms {}]
+          (if (= (count mms) 8)
+            mms
+            (let [{:keys [allergens foods]}    (->> mm
+                                                    (filter #(= 1 (count (:foods %))))
+                                                    first)]
+              (recur (->> mm
+                          (map #(update % :foods disj (first foods)))
+                          (map #(update % :allergens disj (first allergens)))
+                          (remove #(empty? (:allergens %)))
+                          (remove #(empty? (:foods %))))
+                     (merge mms
+                            {(first allergens) (first foods)})))))
+                                        ;      aas (set (vals allergens))
+        ]
+
+#_    (prn allergens)
+#_    (->> (sort-by first allergens)
+         (map second)
+         (map str/trim)
+         (str/join ","))))
 
 (defn part-2 []
   (let [d (data)]))

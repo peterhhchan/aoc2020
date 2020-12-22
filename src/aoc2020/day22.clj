09585247732862a4167a42a6971297768aca1c53
peterhhchan <peterhhchan@gmail.com>
2020-12-22 04:06:29 -0500

    Day22 cleanup

--- src/aoc2020/day22.clj
+++ src/aoc2020/day22.clj
@@ -5,12 +5,11 @@
 (defn toInt [s]
   (Integer/parseInt s))
 
-(defn data []
+(defn decks []
   (let [players (->> (s/split (slurp "data/aoc2020_day22.txt") #"\n\n")
-                     (map (fn [lines]
-                            (->> (s/split-lines lines)
-                                 (drop 1)
-                                 (mapv toInt)))))]
+                     (map #(->> (s/split-lines %)
+                                (drop 1)
+                                (mapv toInt))))]
  {:p1 (first players)
   :p2 (second players)}))
 
@@ -18,47 +17,57 @@
   (Long/parseLong s))
 
 (defn play-game [d1 d2]
-  (loop [[f1 & r1] d1
-         [f2 & r2] d2]
-    (if (and f1 f2)
-      (if (> f1 f2)
-        (recur (concat r1 [f1 f2]) r2)
-        (recur r1 (concat r2 [f2 f1])))
-      (if f1
-        [:p1 (concat [f1] r1) ]
-        [:p2 (concat [f2] r2) ]))))
+  (loop [deck1 d1
+         deck2 d2]
+    (let [c1 (first deck1)
+          c2 (first deck2)]
+      (cond (empty? deck1) [:p2 deck2]
+            (empty? deck2) [:p1 deck1]
+            :else
+            (if (> c1 c2)
+              (recur (conj (subvec deck1 1) c1 c2)
+                     (subvec deck2 1))
+              (recur (subvec deck1 1)
+                     (conj (subvec deck2 1) c2 c1)))))))
+
+(defn score-deck [d]
+  (->> (reverse (range 1 (inc (count d))))
+       (map * d)
+       (reduce +)))
 
 (defn part-1 []
-  (let [d (data)]
-    (play-game (:p1 d) (:p2 d))))
+  (let [{:keys [p1 p2]} (decks)]
+    (-> (play-game p1 p2)
+        (second)
+        (score-deck))))
 
-(def m-mem
+(def mem-game-2
   (memoize
    (fn [player1 player2 ]
      (loop [d1 (vec player1)
             d2 (vec player2)
             games-played #{}]
-       (cond
-             (empty? d2)
-             [:p1 d1]
-             (empty? d1)
-             [:p2 d2]
-             (games-played [d1 d2])
-             [:p1 d1]
+       (cond (empty? d2) {:winner :p1 :deck d1}
+             (empty? d1) {:winner :p2 :deck d2}
+             (games-played [d1 d2]) {:winner :p1 :deck d1}
              :else
-             (let [[f1 & r1] d1
-                   [f2 & r2] d2
-                   sub-game? (and (<= f1 (count r1)) (<= f2 (count r2)))]
-               (if (or (and sub-game? (= :p1 (first (m-mem (take f1 r1) (take f2 r2)))))
-                       (and (not sub-game?) (> f1 f2)))
-                   (recur (concat r1 [f1 f2]) r2 (conj games-played [(vec d1) (vec d2)]))
-                   (recur r1 (concat r2 [f2 f1]) (conj games-played [(vec d1) (vec d2)])))))))))
-
+             (let [f1  (d1 0)
+                   f2  (d2 0)
+                   sub-game? (and (<= f1 (dec (count d1)))
+                                  (<= f2 (dec (count d2))))]
+               (if (or (and sub-game?
+                            (= :p1 (:winner (mem-game-2 (subvec d1 1 (inc f1))
+                                                        (subvec d2 1 (inc f2))))))
+                       (and (not sub-game?)
+                            (> f1 f2)))
+                 (recur (conj (subvec d1 1) f1 f2)
+                        (subvec d2 1)
+                        (conj games-played [d1 d2]))
+                 (recur (subvec d1 1)
+                        (conj (subvec d2 1) f2 f1)
+                        (conj games-played [d1 d2])))))))))
 
 (defn part-2 []
-  (let [d (data)
-        winner    (m-mem (:p1 d) (:p2 d))
-        deck-size (count (second winner))]
-    (->> (map #(vector %1 %2) (second winner) (reverse (range 1 (inc deck-size))))
-         (map (partial apply *))
-         (reduce +))))
+  (let [{:keys [p1 p2]} (data)
+        winner (mem-game-2 p1 p2)]
+    (score-deck (:deck winner))))

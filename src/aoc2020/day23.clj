b2df60d53d0cb04384757428124c18952b6dc8b2
peterhhchan <peterhhchan@gmail.com>
2020-12-23 07:33:36 -0500

    Day23

--- src/aoc2020/day23.clj
+++ src/aoc2020/day23.clj
@@ -18,7 +18,7 @@
   (let [d (data)]))
 
 (def input [1 9 3 4 6 7 2 5 8])
-;;(def input [3 8 9 1 2 5 4 6 7])
+(def input [3 8 9 1 2 5 4 6 7])
 (defn answer-1 [v]
   (->> (concat v v)
        (drop-while #(not= 1 %))
@@ -68,4 +68,86 @@
     (->> (iterate step-faster new-input)
          (drop 100)
          first
+         answer-2)))
+
+(defn take-cups [n {:keys [llist head tail]}]
+  (loop [res     []
+         cur     head
+         counter 0]
+    (if (= n counter)
+      res
+      (recur (conj res cur)
+             (llist cur)
+             (inc counter)))))
+
+(defn cup-list [input]
+  (loop [res {}
+         cur nil
+         [f & r] input]
+    (if f
+      (recur (assoc res f cur) f r)
+      {:llist res
+       :head (last input)
+       :tail (first input)})))
+
+;;[1 9 3 4 6 7 2 5 8]
+;;(def input [3 8 9 1 2 5 4 6 7])
+
+(defn debug-list [cup-list]
+  (vec (take-cups (count (:llist cup-list)) cup-list)))
+
+(defn insert-list [llist pos items]
+;  (prn items)
+  (let [cur (llist pos)]
+    (-> llist
+        (assoc pos (first items))
+        (assoc (last items) cur))))
+
+(defn get-tail [cup-list pos]
+  (if (nil? (cup-list pos))
+    pos
+    (get-tail cup-list (cup-list pos))))
+
+(defn step [{:keys [llist head tail] :as cup-list}]
+  (let [fs           (take-cups 5 cup-list)
+        to-move      (take 3 (drop 1 fs))
+        next-head    (last fs)
+
+        destination  (->> (concat (range (dec head) 0 -1)
+                                  (range (count llist) 0 -1))
+                          (remove (set to-move) )
+                          first)
+        next-seq  (-> llist
+                      (assoc tail head)
+                      (assoc head nil)
+                      (insert-list destination to-move))]
+    {:llist next-seq
+     :head next-head
+     :tail (get-tail next-seq head)}))
+
+(def input [1 9 3 4 6 7 2 5 8])
+;(def input [3 8 9 1 2 5 4 6 7])
+
+#_(defn part-1 []
+  (let [n 9
+        mx (apply max input)
+        new-input (vec (concat input (range (inc mx) (inc n))))
+        ll (cup-list (reverse new-input))]
+    (prn (count new-input))
+    (->> (iterate step ll)
+         (drop 100)
+         first
+         debug-list
+         answer-1)))
+
+(defn part-2 []
+  (let [n 1000000
+        mx (apply max input)
+        new-input (vec (concat input (range (inc mx) (inc n))))
+        ll (cup-list (reverse new-input))]
+    (prn (count new-input))
+    (->> (iterate step ll)
+         (drop 10000000)
+         first
+         debug-list
          answer-2)))

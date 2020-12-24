38997b6bf3c4ac0756e638b4e77c86ea56eba053
peterhhchan <peterhhchan@gmail.com>
2020-12-23 23:55:30 -0500

    Faster 23.  I should just remove the functions :D

--- src/aoc2020/day23.clj
+++ src/aoc2020/day23.clj
@@ -1,25 +1,26 @@
 (ns aoc2020.day23)
 
 (defn build-list [input]
-  (loop [res {}
+  (loop [res (transient {})
          cur nil
          [f & r] input]
     (if f
-      (recur (assoc res f cur) f r)
+      (recur (assoc! res f cur) f r)
       {:llist res
        :head (last input)
        :tail (first input)})))
 
 (defn take-cups [n {:keys [llist head tail]}]
   ;; TODO - make this more idiomatic using iterate
-  (loop [res     []
+  (loop [res     (transient [])
          cur     head
          counter 0]
     (if (= n counter)
-      res
-      (recur (conj res cur)
+      (persistent! res)
+      (recur (conj! res cur)
              (llist cur)
              (inc counter)))))
+
 
 (defn debug-list [cup-list]
   (vec (take-cups (count (:llist cup-list)) cup-list)))
@@ -27,26 +28,33 @@
 (defn insert-list [llist pos items]
   (let [cur (llist pos)]
     (-> llist
-        (assoc pos (first items))
-        (assoc (last items) cur))))
+        (assoc! pos (first items))
+        (assoc! (last items) cur))))
+
+(defn insert-fast! [llist pos items]
+  ;; like `insert-list` but hardcoded to 3 items
+  (let [cur (llist pos)]
+    (-> llist
+        (assoc! pos (items 0))
+        (assoc! (items 2) cur))))
 
 (defn get-tail [cup-list pos]
-  (if (nil? (cup-list pos))
-    pos
-    (get-tail cup-list (cup-list pos))))
+  (if-let [lst (cup-list pos)]
+    (get-tail cup-list lst)
+    pos))
 
 (defn step [{:keys [llist head tail] :as cup-list}]
   (let [fs          (take-cups 5 cup-list)
-        to-move     (take 3 (drop 1 fs))
-        next-head   (last fs)
+        to-move     (subvec fs 1 4)
+        next-head   (fs 4)
         destination (->> (concat (range (dec head) 0 -1)
                                  (range (count llist) 0 -1))
                          (remove (set to-move) )
                           first)
         next-list   (-> llist
-                       (assoc tail head)
-                       (assoc head nil)
-                       (insert-list destination to-move))]
+                       (assoc! tail head)
+                       (assoc! head nil)
+                       (insert-fast! destination to-move))]
     {:llist next-list
      :head  next-head
      :tail  (get-tail next-list head)}))

880a23aae4fd3c96000b4bb890eb41a11fdf4041
peterhhchan <peterhhchan@gmail.com>
2020-12-24 00:48:43 -0500

    Day24!  sub 1k!

--- src/aoc2020/day24.clj
+++ src/aoc2020/day24.clj
@@ -1,18 +1,51 @@
-(ns aoc2020.day24
-  (:require [clojure.string :as s]))
+(ns aoc2020.day24)
 
-(defn data []
+(defn tile [t]
+  (let [f     (frequencies t)
+        ew    (- (get f "e" 0) (f "w" 0))
+        ns    (- (+ (get f "nw" 0) (get f "ne" 0))
+                 (+ (get f "sw" 0) (get f "se" 0)))
+        ew-ns (- (+ (get f "ne" 0) (get f "se" 0))
+                 (+ (get f "nw" 0) (get f "sw" 0)))]
+    [(+ ew (/ ew-ns 2)) ns]))
+
+(defn tiles []
   (->> (slurp "data/aoc2020_day24.txt")
-       s/split-lines))
+       s/split-lines
+       (map #(map first (re-seq #"(se|sw|ne|nw|e|w)" %)))
+       (map tile)))
 
-(defn toInt [s]
-  (Integer/parseInt s))
-
-(defn toLong [s]
-  (Long/parseLong s))
+(defn black? [f] (odd? (second f)))
 
 (defn part-1 []
-  (let [d (data)]))
+  (->> (tiles)
+       (frequencies)
+       (filter black?)
+       count))
+
+(defn neighbors [[x y]]
+  (->> (zipmap ["ne" "nw" "se" "sw" "e" "w"] (range))
+       (map tile)
+       (mapv (fn [[a b]]
+              [(+ a x) (+ b y)]))))
+
+(defn step [blacks]
+  (->> (mapcat neighbors blacks)
+       (frequencies)
+       (filter (fn [[t cnt]]
+                 (or (and (blacks t)
+                          (#{1 2} cnt))
+                     (#{2} cnt))))
+       (map first)
+       set))
 
 (defn part-2 []
-  (let [d (data)]))
+  (->> (tiles)
+       (frequencies)
+       (filter black?)
+       (map first)
+       set
+       (iterate step)
+       (drop 100)
+       first
+       count))
